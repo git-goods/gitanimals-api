22997d16cad2a93d7f450bd120e23b90586c0c8d
devxb <dlwnsdud205@naver.com>
2024-04-15 10:14:44 +0900

    refactor: CORS config를 추가한다

---
+++ src/main/kotlin/org/gitanimals/identity/controller/config/CorsConfig.kt
@@ -0,0 +1,24 @@
+package org.gitanimals.identity.controller.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+internal class CorsConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedHeaders("**")
+            .allowedMethods(*ALLOWED_METHOD_NAMES)
+            .maxAge(3600)
+    }
+
+    companion object {
+        private val ALLOWED_METHOD_NAMES = arrayOf(
+            "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE",
+            "OPTIONS", "PATCH"
+        )
+    }
+}
