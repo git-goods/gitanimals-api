818b456ba3790aa64ea9dffde05758e277ba9c21
devxb <dlwnsdud205@naver.com>
2024-04-15 11:27:19 +0900

    fix: UserFacade에 Service 어노테이션을 추가한다

--- src/main/kotlin/org/gitanimals/identity/app/UserFacade.kt
+++ src/main/kotlin/org/gitanimals/identity/app/UserFacade.kt
@@ -2,7 +2,9 @@ package org.gitanimals.identity.app
 
 import org.gitanimals.identity.domain.User
 import org.gitanimals.identity.domain.UserService
+import org.springframework.stereotype.Service
 
+@Service
 class UserFacade(
     private val userService: UserService,
     private val tokenManager: TokenManager,
