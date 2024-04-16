1525811898d3b9aa58c6deea6e8003e95cbd2693
devxb <dlwnsdud205@naver.com>
2024-04-16 12:22:29 +0900

    refactor: cors 를 필터 방식으로 변경한다

---
+++ src/main/kotlin/org/gitanimals/identity/controller/config/CorsFilter.kt
@@ -0,0 +1,33 @@
+package org.gitanimals.identity.controller.config
+
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
+
+@Component
+class CorsFilter : Filter {
+
+    override fun doFilter(
+        request: ServletRequest,
+        response: ServletResponse,
+        chain: FilterChain
+    ) {
+        (response as HttpServletResponse).allowCors()
+        chain.doFilter(request, response)
+    }
+
+    private fun HttpServletResponse.allowCors(): ServletResponse {
+        this.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        this.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "*")
+        this.addHeader(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600")
+        this.addHeader(
+            HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
+            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        )
+        return this
+    }
+}
