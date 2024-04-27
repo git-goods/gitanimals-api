dd79916804521bbf0ad52d28be19800588396f83
devxb <dlwnsdud205@naver.com>
2024-04-27 22:29:20 +0900

    feat: GotchaController를 구현한다

---
+++ src/main/kotlin/org/gitanimals/gotcha/controller/GotchaController.kt
@@ -0,0 +1,37 @@
+package org.gitanimals.gotcha.controller
+
+import org.gitanimals.gotcha.app.GotchaFacade
+import org.gitanimals.gotcha.controller.response.ErrorResponse
+import org.gitanimals.gotcha.controller.response.GotchaResponse
+import org.gitanimals.gotcha.domain.GotchaType
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class GotchaController(
+    private val gotchaFacade: GotchaFacade,
+) {
+
+    @PostMapping("/gotchas")
+    fun gotcha(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) token: String,
+        @RequestParam(name = "type", defaultValue = "DEFAULT") type: String,
+    ): GotchaResponse {
+        val gotchaType = GotchaType.valueOf(type.uppercase())
+
+        val gotchaResponse = gotchaFacade.gotcha(token, gotchaType)
+
+        return GotchaResponse(gotchaResponse.name, gotchaResponse.point)
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(illegalArgumentException: IllegalArgumentException): ErrorResponse =
+        ErrorResponse.from(illegalArgumentException)
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalStateException(illegalStateException: IllegalStateException): ErrorResponse =
+        ErrorResponse("Oops! Something went wrong.")
+}
---
+++ src/main/kotlin/org/gitanimals/gotcha/controller/response/ErrorResponse.kt
@@ -0,0 +1,11 @@
+package org.gitanimals.gotcha.controller.response
+
+data class ErrorResponse(
+    val message: String,
+) {
+
+    companion object {
+        fun from(exception: Exception): ErrorResponse =
+            ErrorResponse(exception.message ?: exception.localizedMessage)
+    }
+}
