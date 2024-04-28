3e61fdc0279a804f9e7e9367998db55d9ce9c340
devxb <dlwnsdud205@naver.com>
2024-04-28 20:10:28 +0900

    refactor: Gotcha api가 pet의 id를 함께 반환하도록 수정한다

--- src/main/kotlin/org/gitanimals/gotcha/controller/response/GotchaResponse.kt
+++ src/main/kotlin/org/gitanimals/gotcha/controller/response/GotchaResponse.kt
@@ -1,6 +1,7 @@
 package org.gitanimals.gotcha.controller.response
 
 data class GotchaResponse(
+    val id: String,
     val name: String,
     val ratio: String,
 )
--- src/main/kotlin/org/gitanimals/gotcha/domain/response/GotchaResponse.kt
+++ src/main/kotlin/org/gitanimals/gotcha/domain/response/GotchaResponse.kt
@@ -3,6 +3,7 @@ package org.gitanimals.gotcha.domain.response
 import java.util.*
 
 data class GotchaResponse(
+    var id: String? = null,
     val name: String,
     val ratio: String,
     val point: String,
