73277ad0002305901d24c3d59043138851d1ec96
devxb <develxb@gmail.com>
2024-07-10 16:25:59 +0900

    feat: Daily user report기능을 추가한다

--- src/main/kotlin/org/gitanimals/Application.kt
+++ src/main/kotlin/org/gitanimals/Application.kt
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.retry.annotation.EnableRetry
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableSaga
 @EnableRetry
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 class Application {
