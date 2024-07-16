ca7813dfe13341627f8e25b29300ea13482fa3cf
devxb <develxb@gmail.com>
2024-07-16 15:50:24 +0900

    feat: 로그인시 HOME과 ADMIN을 선택해서 할 수 있도록 구현한다

--- src/main/kotlin/org/gitanimals/identity/controller/Oauth2Controller.kt
+++ src/main/kotlin/org/gitanimals/identity/controller/Oauth2Controller.kt
@@ -1,13 +1,11 @@
 package org.gitanimals.identity.controller
 
 import org.gitanimals.identity.app.LoginFacade
+import org.gitanimals.identity.controller.request.RedirectWhenSuccess
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class Oauth2Controller(
@@ -17,25 +15,31 @@ class Oauth2Controller(
 
     @GetMapping("/logins/oauth/github")
     @ResponseStatus(HttpStatus.MOVED_PERMANENTLY)
-    fun redirectToGithub(): ResponseEntity<Unit> {
+    fun redirectToGithub(
+        @RequestHeader(
+            name = "Redirect-When-Success",
+            defaultValue = "HOME"
+        ) redirectWhenSuccess: RedirectWhenSuccess,
+    ): ResponseEntity<Unit> {
         return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
             .header(
                 "Location",
-                "https://github.com/login/oauth/authorize?client_id=$githubClientId"
+                "https://github.com/login/oauth/authorize?client_id=$githubClientId?redirect_uri=${redirectWhenSuccess.callbackUri}"
             )
             .build()
     }
 
-    @GetMapping("/logins/oauth/github/tokens")
+    @GetMapping("/logins/oauth/github/tokens/{redirect-path}")
     @ResponseStatus(HttpStatus.CREATED)
     fun login(
         @RequestParam("code") code: String,
+        @PathVariable("redirect-path") redirectWhenSuccess: RedirectWhenSuccess,
     ): ResponseEntity<Unit> {
         val token = loginFacade.login(code)
         return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT)
             .header(
                 "Location",
-                "https://www.gitanimals.org/jwt?jwt=$token"
+                redirectWhenSuccess.successUriWithToken(token)
             )
             .build()
     }
---
+++ src/main/kotlin/org/gitanimals/identity/controller/request/RedirectWhenSuccess.kt
@@ -0,0 +1,18 @@
+package org.gitanimals.identity.controller.request
+
+enum class RedirectWhenSuccess(
+    val callbackUri: String,
+    private val successUri: String,
+) {
+    HOME(
+        "https://api.gitanimals.org/logins/oauth/github/tokens/HOME",
+        "https://www.gitanimals.org/jwt={jwt}",
+    ),
+    ADMIN(
+        "https://api.gitanimals.org/logins/oauth/github/tokens/ADMIN",
+        "https://admin.gitanimals.org/jwt={jwt}",
+    ),
+    ;
+
+    fun successUriWithToken(jwt: String): String = successUri.replace("{jwt}", jwt)
+}
