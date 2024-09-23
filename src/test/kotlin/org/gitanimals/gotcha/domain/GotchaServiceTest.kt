88ec50d39ece52d233145466f7c69ab732932722
devxb <develxb@gmail.com>
2024-09-24 01:26:18 +0900

    fix: jwt token 재등록

--- src/test/kotlin/org/gitanimals/gotcha/domain/GotchaServiceTest.kt
+++ src/test/kotlin/org/gitanimals/gotcha/domain/GotchaServiceTest.kt
@@ -4,6 +4,7 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
 import org.junit.jupiter.api.DisplayName
 import org.springframework.test.context.ContextConfiguration
+import java.util.*
 
 @ContextConfiguration(
     classes = [
@@ -19,7 +20,6 @@ internal class GotchaServiceTest(
         context("GotchaType으로 default를 입력받으면,") {
             it("random한 GotchaResponse를 응답한다.") {
                 val result = gotchaService.gotcha(1000L, GotchaType.DEFAULT)
-
                 result.shouldNotBeNull()
             }
         }
