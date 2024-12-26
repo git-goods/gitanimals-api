aeff7413c985c2d19630ddc3498d2dd70a56af53
devxb <develxb@gmail.com>
2024-12-26 18:17:13 +0900

    refactor: 뽑기 API v2, v1 fade out

--- src/main/kotlin/org/gitanimals/gotcha/controller/GotchaController.kt
+++ src/main/kotlin/org/gitanimals/gotcha/controller/GotchaController.kt
@@ -1,10 +1,8 @@
 package org.gitanimals.gotcha.controller
 
-import org.gitanimals.gotcha.app.GotchaFacade
 import org.gitanimals.gotcha.app.GotchaFacadeV3
 import org.gitanimals.gotcha.app.response.GotchaResponseV3
 import org.gitanimals.gotcha.controller.response.ErrorResponse
-import org.gitanimals.gotcha.controller.response.GotchaResponse
 import org.gitanimals.gotcha.domain.GotchaType
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
@@ -12,41 +10,8 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 class GotchaController(
-    private val gotchaFacade: GotchaFacade,
     private val gotchaFacadeV3: GotchaFacadeV3,
 ) {
-
-    @PostMapping("/gotchas")
-    fun gotcha(
-        @RequestHeader(HttpHeaders.AUTHORIZATION) token: String,
-        @RequestParam(name = "type", defaultValue = "DEFAULT") type: String,
-    ): GotchaResponse {
-        val gotchaType = GotchaType.valueOf(type.uppercase())
-
-        val gotchaResponses = gotchaFacade.gotcha(token, gotchaType, 1)
-
-        return GotchaResponse(
-            gotchaResponses[0].name,
-            gotchaResponses[0].ratio
-        )
-    }
-
-    @PostMapping(path = ["/gotchas"], headers = ["Api-Version=2"])
-    fun gotchaV2(
-        @RequestHeader(HttpHeaders.AUTHORIZATION) token: String,
-        @RequestParam(name = "type", defaultValue = "DEFAULT") type: String,
-        @RequestParam(name = "count", defaultValue = "1") count: Int,
-    ): Map<String, List<GotchaResponse>> {
-        val gotchaType = GotchaType.valueOf(type.uppercase())
-
-        val gotchaResponses = gotchaFacade.gotcha(token, gotchaType, count)
-
-        return mapOf(
-            "gotchaResults" to gotchaResponses.map {
-                GotchaResponse(it.name, it.ratio)
-            }.toList()
-        )
-    }
 
     @PostMapping(path = ["/gotchas"], headers = ["Api-Version=3"])
     fun gotchaV3(
---
+++ src/test/kotlin/org/gitanimals/gotcha/app/GotchaFacadeV3Test.kt
@@ -0,0 +1,137 @@
+package org.gitanimals.gotcha.app
+
+import io.jsonwebtoken.JwtException
+import io.kotest.assertions.nondeterministic.eventually
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.assertions.throwables.shouldThrowWithMessage
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.mockk.every
+import org.gitanimals.gotcha.app.response.UserResponse
+import org.gitanimals.gotcha.domain.GotchaService
+import org.gitanimals.gotcha.domain.GotchaType
+import org.gitanimals.gotcha.infra.RestRenderApi
+import org.junit.jupiter.api.DisplayName
+import org.rooftop.netx.meta.EnableSaga
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import kotlin.time.Duration.Companion.seconds
+
+@EnableSaga
+@DataJpaTest
+@ContextConfiguration(
+    classes = [
+        RedisContainer::class,
+        MockServerConfiguration::class,
+        SagaCapture::class,
+        GotchaService::class,
+        GotchaFacadeV3::class,
+    ]
+)
+@EntityScan("org.gitanimals.gotcha")
+@EnableJpaRepositories("org.gitanimals.gotcha")
+@TestPropertySource("classpath:test.properties")
+@DisplayName("GotchaFacadeV3 클래스의")
+internal class GotchaFacadeV3Test(
+    private val sagaCapture: SagaCapture,
+    private val gotchaFacadeV3: GotchaFacadeV3,
+    private val userApi: UserApi,
+    private val renderApi: RenderApi,
+) : DescribeSpec({
+
+    describe("gotcha 메소드는") {
+        context("token에 해당하는 유저와 GotchaType에 해당하는 Gotcha가 존재한다면,") {
+            every { userApi.getUserByToken(any()) } returns richUser
+            every {
+                renderApi.addPersonas(
+                    any(),
+                    any(),
+                    any()
+                )
+            } returns listOf(addPersonaResponse.id)
+
+            it("gotcha를 성공한다.") {
+                val gotchaResponse = gotchaFacadeV3.gotcha("token", GotchaType.DEFAULT, 1)
+
+                gotchaResponse.shouldNotBeNull()
+            }
+        }
+
+        context("token에 해당하는 유저가 충분한 돈을 갖고있지 않다면,") {
+            every { userApi.getUserByToken(any()) } returns poorUser
+
+            it("IllegalArgumentException을 던진다.") {
+                shouldThrowWithMessage<IllegalArgumentException>("Not enough point \"0\" <= \"1000\"") {
+                    gotchaFacadeV3.gotcha("token", GotchaType.DEFAULT, 1)
+                }
+            }
+        }
+
+        context("포인트가 충분한 유저가 Gotcha를 10번 연속 한다면,") {
+            every { userApi.getUserByToken(any()) } returns richUser
+
+            every { renderApi.addPersonas(any(), any(), any()) } returns listOf(
+                addPersonaResponse.id,
+                addPersonaResponse.id,
+                addPersonaResponse.id,
+                addPersonaResponse.id,
+                addPersonaResponse.id,
+                addPersonaResponse.id,
+                addPersonaResponse.id,
+                addPersonaResponse.id,
+                addPersonaResponse.id,
+                addPersonaResponse.id,
+            )
+
+
+            it("gotcha를 성공한다.") {
+                val gotchaResponse = gotchaFacadeV3.gotcha("token", GotchaType.DEFAULT, 10)
+
+                gotchaResponse.shouldNotBeNull()
+            }
+        }
+
+        context("addPersona 중에 실패하면,") {
+            every { userApi.getUserByToken(any()) } returns richUser
+            every {
+                renderApi.addPersonas(
+                    any(),
+                    any(),
+                    any()
+                )
+            } throws IllegalArgumentException("for test")
+
+            it("point를 다시 증가시킨다.") {
+                shouldThrow<IllegalArgumentException> {
+                    gotchaFacadeV3.gotcha("token", GotchaType.DEFAULT, 1)
+                }
+
+                eventually(5.seconds) {
+                    sagaCapture.rollbackCountShouldBe(1)
+
+                }
+            }
+        }
+
+        context("JwtException이 발생하면,") {
+            every { userApi.getUserByToken(any()) } throws JwtException("for test")
+
+            it("10초안에 예외를 반환한다") {
+                shouldThrow<JwtException> {
+                    gotchaFacadeV3.gotcha("token", GotchaType.DEFAULT, 1)
+                }
+            }
+        }
+    }
+}) {
+
+    private companion object {
+        private val richUser = UserResponse("1", "rich_guy", "100000", "image-url.png")
+        private val poorUser = UserResponse("1", "poor_guy", "0", "image-url.png")
+        private val addPersonaResponse =
+            RestRenderApi.PersonaResponse("1", "GOOSE", "1", true, "0.1")
+    }
+}
