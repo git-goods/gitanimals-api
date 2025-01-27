15a956c5c9f6dc62da25463e8c21b7538d058b4d
devxb <develxb@gmail.com>
2025-01-27 19:33:35 +0900

    fix: commit으로 포인트가 지급되지 않는 버그를 수정한다

--- src/main/kotlin/org/gitanimals/identity/saga/GivePointHandlers.kt
+++ src/main/kotlin/org/gitanimals/identity/saga/GivePointHandlers.kt
@@ -5,17 +5,22 @@ import org.gitanimals.identity.domain.UserService
 import org.rooftop.netx.api.SagaCommitEvent
 import org.rooftop.netx.api.SagaCommitListener
 import org.rooftop.netx.meta.SagaHandler
+import org.slf4j.LoggerFactory
 
 @SagaHandler
 class GivePointHandlers(
     private val userService: UserService,
 ) {
 
+    private val logger = LoggerFactory.getLogger(this::class.simpleName)
+
     @SagaCommitListener(GavePoint::class)
     fun givePointHandler(sagaCommitEvent: SagaCommitEvent) {
         val gavePoint = sagaCommitEvent.decodeEvent(GavePoint::class)
         runCatching {
             userService.givePoint(gavePoint.username, gavePoint.point, "FOR_COMMIT")
+        }.onFailure {
+            logger.error("Cannot give point to user. username: \"${gavePoint.username}\", point: \"${gavePoint.point}\"")
         }
     }
 }
