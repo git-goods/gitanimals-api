ba999028ae343fc2554126bfcca77cb163f6252a
devxb <develxb@gmail.com>
2025-03-29 23:41:46 +0900

    feat: 퀴즈 승인이 거절되면, 포인트 지급을 철회한다

--- src/main/kotlin/org/gitanimals/quiz/app/DenyQuizFacade.kt
+++ src/main/kotlin/org/gitanimals/quiz/app/DenyQuizFacade.kt
@@ -1,5 +1,7 @@
 package org.gitanimals.quiz.app
 
+import org.gitanimals.core.IdGenerator
+import org.gitanimals.quiz.app.CreateQuizFacade.Companion.CREATE_QUIZ_PRICE
 import org.gitanimals.quiz.domain.not_approved.NotApprovedQuizService
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service
 
 @Service
 class DenyQuizFacade(
+    private val identityApi: IdentityApi,
     private val notApprovedQuizService: NotApprovedQuizService,
     @Value("\${quiz.approve.token}") private val approveToken: String,
 ) {
@@ -20,6 +23,20 @@ class DenyQuizFacade(
             message
         }
 
+        val notApprovedQuiz = runCatching {
+            notApprovedQuizService.getById(notApprovedQuizId)
+        }.getOrElse { return }
+
         notApprovedQuizService.deleteQuizById(notApprovedQuizId)
+
+        runCatching {
+            identityApi.decreaseUserPointsById(
+                userId = notApprovedQuiz.userId,
+                point = CREATE_QUIZ_PRICE,
+                idempotencyKey = IdGenerator.generate().toString(),
+            )
+        }.getOrElse {
+            logger.error("[DenyQuizFacade] Cannot decrease point. userId: \"${notApprovedQuiz.userId}\", point: \"$CREATE_QUIZ_PRICE\"")
+        }
     }
 }
