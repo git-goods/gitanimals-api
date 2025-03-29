2a569d91251ccbc915352411a1ed62e4b919f5eb
devxb <develxb@gmail.com>
2025-03-29 18:41:10 +0900

    fix: SlackInteractController를 APPLICATION_FORM_URLENCODED_VALUE 처리할 수 있도록 수정

--- src/main/kotlin/org/gitanimals/notification/controller/SlackInteractController.kt
+++ src/main/kotlin/org/gitanimals/notification/controller/SlackInteractController.kt
@@ -1,22 +1,27 @@
 package org.gitanimals.notification.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.gitanimals.notification.app.SlackInteractRequestDispatcher
 import org.gitanimals.notification.app.request.SlackInteractRequest
 import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SlackInteractController(
+    private val objectMapper: ObjectMapper,
     private val slackInteractRequestDispatcher: SlackInteractRequestDispatcher,
 ) {
 
     private val logger = LoggerFactory.getLogger(this::class.simpleName)
 
-    @PostMapping("/slack/interact")
-    fun dispatchSlackInteracts(@RequestBody slackInteractRequest: SlackInteractRequest) {
-        logger.info("SlackInteractRequest: $slackInteractRequest")
-        slackInteractRequestDispatcher.dispatch(slackInteractRequest)
+    @PostMapping("/slack/interact", consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
+    fun dispatchSlackInteracts(@RequestParam("payload") payload: String) {
+        logger.info("payload: $payload")
+
+        val slackRequest = objectMapper.readValue(payload, SlackInteractRequest::class.java)
+        slackInteractRequestDispatcher.dispatch(slackRequest)
     }
 }
