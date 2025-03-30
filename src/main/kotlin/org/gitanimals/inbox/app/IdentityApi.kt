cdaef58eacf5a9758fe1a0b3b5c3d00124f60d8f
devxb <develxb@gmail.com>
2025-03-30 19:11:21 +0900

    refactor: inbox restclient -> httpclient로 수정한다

--- src/main/kotlin/org/gitanimals/inbox/app/IdentityApi.kt
+++ src/main/kotlin/org/gitanimals/inbox/app/IdentityApi.kt
@@ -1,7 +1,10 @@
 package org.gitanimals.inbox.app
 
+import org.springframework.web.service.annotation.GetExchange
+
 fun interface IdentityApi {
 
+    @GetExchange("/users")
     fun getUserByToken(token: String): UserResponse
 
     data class UserResponse(
