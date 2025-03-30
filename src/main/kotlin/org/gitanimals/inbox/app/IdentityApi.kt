dbbe6007c800ddd19d13996f7e26a56ab9f4adbb
devxb <develxb@gmail.com>
2025-03-30 19:32:19 +0900

    fix: Inbox HttpClient에 누락된 헤더 지정자를 추가한다

--- src/main/kotlin/org/gitanimals/inbox/app/IdentityApi.kt
+++ src/main/kotlin/org/gitanimals/inbox/app/IdentityApi.kt
@@ -1,11 +1,13 @@
 package org.gitanimals.inbox.app
 
+import org.springframework.http.HttpHeaders
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.service.annotation.GetExchange
 
 fun interface IdentityApi {
 
     @GetExchange("/users")
-    fun getUserByToken(token: String): UserResponse
+    fun getUserByToken(@RequestHeader(HttpHeaders.AUTHORIZATION) token: String): UserResponse
 
     data class UserResponse(
         val id: String,
