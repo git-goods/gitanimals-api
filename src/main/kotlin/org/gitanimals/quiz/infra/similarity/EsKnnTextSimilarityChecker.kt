e54bdfdd748f216f42aedbb08fcf0cf4a886ad3d
devxb <develxb@gmail.com>
2025-09-28 22:53:20 +0900

    refactor: 유사도 0.35 -> 0.8로 조정한다

--- src/main/kotlin/org/gitanimals/quiz/infra/similarity/EsKnnTextSimilarityChecker.kt
+++ src/main/kotlin/org/gitanimals/quiz/infra/similarity/EsKnnTextSimilarityChecker.kt
@@ -24,7 +24,7 @@ class EsKnnTextSimilarityChecker(
             .withKnnSearches {
                 it.field(QuizSimilarity::vector.name)
                 it.queryVector(embeddingResponse.data.first().embedding)
-                it.similarity(0.35F)
+                it.similarity(0.8F)
                 it.k(MAX_RETURN_KNN_SIZE)
                 it.numCandidates(MAX_RETURN_KNN_SIZE * 5)
             }.build()
